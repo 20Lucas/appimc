c33fd385e4d3a3f17e32a4e278479a03967fe461
20Lucas <Lucas.novais5@etec.sp.gob.br>
2023-05-20 00:31:55 +0000

    appImc

--- App.js
+++ App.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
   },
   resultado:{
     alignSelf:"center",
-    color:"#A14837",
+    color:"#EEAB75",
     fontSize:45,
     fontWeight:'bold',
     padding: 6,
